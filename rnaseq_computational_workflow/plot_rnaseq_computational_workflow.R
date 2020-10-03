9840e81d93bf6aa87922bde2b38fa082397060b6
Christian Brueffer <christian@brueffer.io>
2020-10-04 00:16:38 +0100

    Add two RNA-seq workflows.

---
+++ rnaseq_computational_workflow/plot_rnaseq_computational_workflow.R
@@ -0,0 +1,20 @@
+#!/usr/bin/env Rscript
+
+#
+# Flow chart for the SCAN-B dUTP strand-specific RNA-seq library prep.
+#
+
+# Inspiration:
+# https://mikeyharper.uk/flowcharts-in-r-using-diagrammer/
+# https://github.com/dr-harper/blog/blob/master/content/post/2018-05-01-flowcharts-in-r-using-diagrammer/planningGrouping.gv
+
+library(magrittr)
+library(DiagrammeR)
+library(DiagrammeRsvg)
+library(rsvg)
+
+DiagrammeR::grViz("rnaseq_general_workflow.gv")
+
+#export_svg(DiagrammeR::grViz("rnaseq-general-workflow.gv")) %>%
+#    charToRaw %>%
+#    rsvg_pdf("rnaseq-general-workflow.pdf")
